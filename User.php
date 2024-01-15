bd03b9df16b4cd1723277237f0c3131986c376f2
angel-putz <angel.putz@laplateforme.io>
2024-01-15 16:38:29 +0100

    dernière update de 15/01/24

--- User.php
+++ User.php
@@ -27,78 +27,42 @@ class User {
     }
 
     public function register($login, $email, $firstname, $lastname , $password) {
-        
-        $servername = "localhost";
-        $username = "root";
-        $passwordBDD = "";
-        $database = "classes";
-        
-        
-
-
-
-        $mysqli = new mysqli($servername, $username, $passwordBDD, $database);
-
-        $mysqli ->query("INSERT INTO `utilisateurs` (`login`, `password`, `email`, `firstname`, `lastname`) VALUES ('$login', '$password', '$email', '$firstname', '$lastname')");
+        $mysqli = new mysqli($this->servername, $this->username, $this->passwordBDD, $this->database);
+        $mysqli->query("INSERT INTO `utilisateurs` (`login`, `password`, `email`, `firstname`, `lastname`) VALUES ('$login', '$password', '$email', '$firstname', '$lastname')");
         return $mysqli;
+        echo "Vous êtes inscrit !";
     }
 
     public function connect($login , $password) {
-        $servername = "localhost";
-        $username = "root";
-        $password = "";
-        $database = "classes";
-
-
-        $login = $_POST['login'];
-        $password = $_POST['password'];
-
-        $mysqli = new mysqli($servername, $username, $password, $database);
-
-        $mysqli ->query("SELECT * FROM `utilisateurs` WHERE `login` = '$login' AND `password` = '$password'");
+        $mysqli = new mysqli($this->servername, $this->username, $this->passwordBDD, $this->database);
+        $mysqli->query("SELECT * FROM `utilisateurs` WHERE `login` = '$login' AND `password` = '$password'");
+        echo "Vous êtes connecté !";
         return $mysqli;
-
     }
 
     public function disconnect() {
+        session_unset();
         session_destroy();
+        echo "Vous êtes déconnecté !";
     }
 
     public function delete() {
-        $servername = "localhost";
-        $username = "root";
-        $password = "";
-        $database = "classes";
-
-        $login = $_SESSION['login'];
-        $password = $_SESSION['password'];
-
-        $mysqli = new mysqli($servername, $username, $password, $database);
-
-        $mysqli ->query("DELETE FROM `utilisateurs` WHERE `login` = '$login' AND `password` = '$password'");
+        $mysqli = new mysqli($this->servername, $this->username, $this->passwordBDD, $this->database);
+        $login = $_POST['login'];
+        $password = $_POST['password'];
+        $mysqli->query("DELETE FROM `utilisateurs` WHERE `login` = '$login' AND `password` = '$password'");
+        //$mysqli->query("DELETE FROM `utilisateurs` WHERE `login` = '$login' AND `password` = '$password'");
+        echo "Votre compte a été supprimé !";
     }
 
     public function update($login ,$password ,$email , $firstname , $lastname){
-        $servername = "localhost";
-        $username = "root";
-        $passwordBDD = "";
-        $database = "classes";
-
-        $login = $_POST['login'];
-        $password = $_POST['password'];
-        $email = $_POST['email'];
-        $firstname = $_POST['firstname'];
-        $lastname = $_POST['lastname'];
-
-        $mysqli = new mysqli($servername, $username, $passwordBDD, $database);
-
-        $mysqli2 = new mysqli($servername, $username, $passwordBDD, $database);
-
-        $id = $mysqli2 ->query("SELECT 'id' FROM `utilisateurs` WHERE `login` = '$login' AND `password` = '$password'");
-
-
-        $mysqli ->query("UPDATE `utilisateurs` SET `login` = '$login', `password` = '$password', `email` = '$email', `firstname` = '$firstname', `lastname` = '$lastname' WHERE `utilisateurs`.`id` = $id");
-
+        $mysqli = new mysqli($this->servername, $this->username, $this->passwordBDD, $this->database);
+        $mysqli2 = new mysqli($this->servername, $this->username, $this->passwordBDD, $this->database);
+        $id = $mysqli2->query("SELECT 'id' FROM `utilisateurs` WHERE `login` = '$login' AND `password` = '$password'");
+       // $mysqli->query("UPDATE `utilisateurs` SET `login` = '2', `password` = '2', `email` = '2', `firstname` = '2', `lastname` = '2' WHERE `utilisateurs`.`id` = '7'");
+        $mysqli->query("UPDATE `utilisateurs` SET `login` = '$login', `password` = '$password', `email` = '$email', `firstname` = '$firstname', `lastname` = '$lastname' WHERE `utilisateurs`.`id` = $id");
+        var_dump($mysqli);
+        echo "Votre compte a été mis à jour !";
     }
 
     public function isConnected() {
@@ -109,51 +73,58 @@ class User {
         }
     }
 
-    public function getAllInfos() {
-        $servername = "localhost";
-        $username = "root";
-        $password = "";
-        $database = "classes";
-
-        $login = $_SESSION['login'];
-        $password = $_SESSION['password'];
-
-
-        $mysqli = new mysqli($servername, $username, $password, $database);
-
-        $mysqli ->query("SELECT * FROM `utilisateurs` WHERE `login` = '$login' AND `password` = '$password'");
-        
-        return $mysqli;
+    public function getAllInfos($login , $password) {
+        $mysqli = new mysqli($this->servername, $this->username, $this->passwordBDD, $this->database);
+        $mysqli->query("SELECT * FROM `utilisateurs` WHERE `login` = '$login' AND `password` = '$password'");
+        echo "Voici vos informations !" . $mysqli;
     }
 
-    public function getLogin(){
-        $login = $_SESSION['login'];
-        return $login;
+    public function getLogin($email){
+        $mysqli = new mysqli($this->servername, $this->username, $this->passwordBDD, $this->database);
+        $login=$mysqli->query("SELECT 'login' FROM `utilisateurs` WHERE `email` = '$email'");
+        echo "Voici votre login ! " . $login;
     }
 
-    public function getEmail(){
-        $email = $_SESSION['email'];
-        return $email;
+    public function getEmail($login){
+        $mysqli = new mysqli($this->servername, $this->username, $this->passwordBDD, $this->database);
+        $email=$mysqli->query("SELECT 'email' FROM `utilisateurs` WHERE `login` = '$login''");
+
+        echo "Voici votre email !" . $email;
     }
-    public function getFirstname(){
-        $firstname = $_SESSION['firstname'];
-        return $firstname;
+    public function getFirstname($login){
+        $mysqli = new mysqli($this->servername, $this->username, $this->passwordBDD, $this->database);
+        $firstname=$mysqli->query("SELECT 'firstname' FROM `utilisateurs` WHERE `login` = '$login''");
+
+        echo "Voici votre prénom !". $firstname;
     }
-    public function getLastname(){
-        $lastname = $_SESSION['lastname'];
-        return $lastname;
+    public function getLastname($login){
+        $mysqli = new mysqli($this->servername, $this->username, $this->passwordBDD, $this->database);
+        $lastname=$mysqli->query("SELECT 'lastname' FROM `utilisateurs` WHERE `login` = '$login''");
+        echo "Voici votre nom !" . $lastname;
     }
 }
-$login = $_POST['login'];
-$password = $_POST['password'];
-$email = $_POST['email'];
-$firstname = $_POST['firstname'];
-$lastname = $_POST['lastname'];
 
-$user = new User($login, $password, $email, $firstname, $lastname);
-echo $user->register($login, $email, $firstname, $lastname , $password);
-echo $user->connect($login , $password);
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $login = $_POST['login'];
+    $password = $_POST['password'];
+    $email = $_POST['email'];
+    $firstname = $_POST['firstname'];
+    $lastname = $_POST['lastname'];
 
+    $user = new User($login, $password, $email, $firstname, $lastname);
+  //  $user->register($login, $email, $firstname, $lastname , $password);
+   // $user->connect($login , $password);
+   // $user->connect(2 , 2);
+    //$user->disconnect();
+ //   $user->delete();
+    //$user->update(2 ,2 ,2, 2 , 2);
+    //$user->isConnected();
+   // $user->getAllInfos();
+    $user->getLogin(2);
+  //  $user->getEmail($email);
+    //$user->getFirstname();
+    //$user->getLastname();
+}
 
 ?>
 
@@ -165,12 +136,13 @@ echo $user->connect($login , $password);
     <title>Document</title>
 </head>
 <body>
-    <foùrm method="post">
+    <form method="post">
         <input type="text" name="login" placeholder="login">
         <input type="password" name="password" placeholder="password">
         <input type="email" name="email" placeholder="email">
         <input type="text" name="firstname" placeholder="firstname">
         <input type="text" name="lastname" placeholder="lastname">
         <input type="submit" name="submit" value="submit">
+    </form>
 </body>
 </html>
