9c064e4f06a9a272055d2e03edbddb79129c4f1e
angel-putz <angel.putz@laplateforme.io>
2024-01-16 10:11:08 +0100

    upadte

--- User.php
+++ User.php
@@ -1,5 +1,10 @@
 <?php
 session_start();
+$login = $_POST['login'];
+$password = $_POST['password'];
+$email = $_POST['email'];
+
+$_SESSION['login'] = $login;
 include "database.php";
 
 class User {
@@ -27,21 +32,36 @@ class User {
     }
 
     public function register($login, $email, $firstname, $lastname , $password) {
+
         $mysqli = new mysqli($this->servername, $this->username, $this->passwordBDD, $this->database);
         $mysqli->query("INSERT INTO `utilisateurs` (`login`, `password`, `email`, `firstname`, `lastname`) VALUES ('$login', '$password', '$email', '$firstname', '$lastname')");
-        return $mysqli;
+        
         echo "Vous êtes inscrit !";
     }
 
     public function connect($login , $password) {
         $mysqli = new mysqli($this->servername, $this->username, $this->passwordBDD, $this->database);
-        $mysqli->query("SELECT * FROM `utilisateurs` WHERE `login` = '$login' AND `password` = '$password'");
-        echo "Vous êtes connecté !";
-        return $mysqli;
+        $result = $mysqli->query("SELECT * FROM `utilisateurs` WHERE `login` = '$login' AND `password` = '$password'");
+        $_SESSION['login'] = $login;
+        $_SESSION['password'] = $password;
+
+        
+        if ($result->num_rows > 0) {
+            $user = $result->fetch_assoc();
+            $this->login = $user['login'];
+            $this->password = $user['password'];
+            $this->email = $user['email'];
+            $this->firstname = $user['firstname'];
+            $this->lastname = $user['lastname'];
+            echo "Vous êtes connecté ! Bienvenue " . $this->firstname . " " . $this->lastname . " !";
+            return true;
+        } else {
+            echo "Erreur de connexion !";
+            return false;
+        }
     }
 
     public function disconnect() {
-        session_unset();
         session_destroy();
         echo "Vous êtes déconnecté !";
     }
@@ -66,41 +86,80 @@ class User {
     }
 
     public function isConnected() {
-        if (isset($_SESSION['login']) && isset($_SESSION['password'])) {
+
+        if (isset($_SESSION['login'])) {
+            echo "Vous êtes connecté !";
             return true;
         } else {
+            echo "Vous n'êtes pas connecté !";
             return false;
         }
     }
 
     public function getAllInfos($login , $password) {
         $mysqli = new mysqli($this->servername, $this->username, $this->passwordBDD, $this->database);
-        $mysqli->query("SELECT * FROM `utilisateurs` WHERE `login` = '$login' AND `password` = '$password'");
-        echo "Voici vos informations !" . $mysqli;
+        $result = $mysqli->query("SELECT * FROM `utilisateurs` WHERE `login` = '$login' AND `password` = '$password'");
+    
+        if ($result->num_rows > 0) {
+            $user = $result->fetch_assoc();
+            echo "Voici vos informations !", "<br>" ;
+            echo "Login: " . $user['login'] , "<br>" ;
+            echo "Email: " . $user['email'], "<br>" ;
+            echo "Firstname: " . $user['firstname'], "<br>" ;
+            echo "Lastname: " . $user['lastname'], "<br>" ;
+        } else {
+            echo "Aucun utilisateur trouvé avec ce login et ce mot de passe.";
+        }
     }
 
     public function getLogin($email){
         $mysqli = new mysqli($this->servername, $this->username, $this->passwordBDD, $this->database);
-        $login=$mysqli->query("SELECT 'login' FROM `utilisateurs` WHERE `email` = '$email'");
-        echo "Voici votre login ! " . $login;
+        $result = $mysqli->query("SELECT * FROM `utilisateurs` WHERE `email` = '$email'");
+    
+        if ($result->num_rows > 0) {
+            $user = $result->fetch_assoc();
+            echo "Voici vos login !", "<br>" ;
+            echo "Login: " . $user['login'] , "<br>" ;
+        } else {
+            echo "Aucun utilisateur trouvé";
+        }
     }
 
     public function getEmail($login){
         $mysqli = new mysqli($this->servername, $this->username, $this->passwordBDD, $this->database);
-        $email=$mysqli->query("SELECT 'email' FROM `utilisateurs` WHERE `login` = '$login''");
-
-        echo "Voici votre email !" . $email;
+        $result = $mysqli->query("SELECT * FROM `utilisateurs` WHERE `login` = '$login'");
+    
+        if ($result->num_rows > 0) {
+            $user = $result->fetch_assoc();
+            echo "Voici vos informations !", "<br>" ;
+            echo "Email: " . $user['email'], "<br>" ;
+        } else {
+            echo "Aucun utilisateur trouvé";
+        }
     }
     public function getFirstname($login){
         $mysqli = new mysqli($this->servername, $this->username, $this->passwordBDD, $this->database);
-        $firstname=$mysqli->query("SELECT 'firstname' FROM `utilisateurs` WHERE `login` = '$login''");
-
-        echo "Voici votre prénom !". $firstname;
+        $result = $mysqli->query("SELECT * FROM `utilisateurs` WHERE `login` = '$login'");
+    
+        if ($result->num_rows > 0) {
+            $user = $result->fetch_assoc();
+            echo "Voici vos informations !", "<br>" ;
+            echo "Firstname: " . $user['firstname'], "<br>" ;
+        } else {
+            echo "Aucun utilisateur trouvé";
+        }
     }
     public function getLastname($login){
         $mysqli = new mysqli($this->servername, $this->username, $this->passwordBDD, $this->database);
-        $lastname=$mysqli->query("SELECT 'lastname' FROM `utilisateurs` WHERE `login` = '$login''");
-        echo "Voici votre nom !" . $lastname;
+        $result = $mysqli->query("SELECT * FROM `utilisateurs` WHERE `login` = '$login'");
+    
+        if ($result->num_rows > 0) {
+            $user = $result->fetch_assoc();
+            echo "Voici vos informations !", "<br>" ;
+            echo "Lastname: " . $user['lastname'], "<br>" ;
+        } else {
+            echo "Aucun utilisateur trouvé";
+        }
     }
 }
 
@@ -111,19 +170,19 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $firstname = $_POST['firstname'];
     $lastname = $_POST['lastname'];
 
-    $user = new User($login, $password, $email, $firstname, $lastname);
-  //  $user->register($login, $email, $firstname, $lastname , $password);
+    $user = new User($login, $password, $email, $firstname, $lastname);  
+   // $user->register($login, $email, $firstname, $lastname , $password); 
    // $user->connect($login , $password);
-   // $user->connect(2 , 2);
+    //$user->connect(2 , 2);
     //$user->disconnect();
  //   $user->delete();
     //$user->update(2 ,2 ,2, 2 , 2);
-    //$user->isConnected();
-   // $user->getAllInfos();
-    $user->getLogin(2);
-  //  $user->getEmail($email);
-    //$user->getFirstname();
-    //$user->getLastname();
+   // $user->isConnected();
+   // $user->getAllInfos($login , $password);
+   // $user->getLogin($email);
+  //  $user->getEmail($login);
+    //$user->getFirstname($login);
+    //$user->getLastname($login);
 }
 
 ?>
@@ -139,7 +198,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <form method="post">
         <input type="text" name="login" placeholder="login">
         <input type="password" name="password" placeholder="password">
-        <input type="email" name="email" placeholder="email">
+        <input type="text" name="email" placeholder="email">
         <input type="text" name="firstname" placeholder="firstname">
         <input type="text" name="lastname" placeholder="lastname">
         <input type="submit" name="submit" value="submit">
